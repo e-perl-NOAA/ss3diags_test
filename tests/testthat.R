85de42b0e4e3cc0f593052eb38627835153bb3b8
MOshima-PIFSC <megumi.oshima@noaa.gov>
2022-06-20 10:34:57 -0400

    initial workflow

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,5 +2,12 @@ library(testthat)
 library(ss3diags)
 
 test_example_path <- system.file("data", package = "ss3diags")
+test_models_path <- system.file("inst/extdata/test-models-repo", package = "ss3diags")
+
+check_ex_models <- function() {
+  if (test_models_path == "") {
+    skip("SS test models not available")
+  }
+}
 
 test_check("ss3diags")
